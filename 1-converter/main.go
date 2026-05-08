88ec48a60c8ea4c96331566db25d9b293b8cfd94
jnkbldgn <buldygin.em@gmail.com>
2026-05-08 20:16:25 +0300

    implement input/output

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	const USD_EUR = 0.85
-	const USD_RUB = 74.65
-	const EUR_RUB = (1 / USD_EUR) * USD_RUB
+	var USD_EUR float64
+	var USD_RUB float64
+
+	readInput("Введите курс USD к EUR: ", &USD_EUR)
+	readInput("Введите курс USD к RUB: ", &USD_RUB)
+
+	var EUR_RUB = (1 / USD_EUR) * USD_RUB
+
+	printValue(EUR_RUB)
+}
+
+func readInput(preview string, numeric *float64) {
+	fmt.Print(preview)
+	_, err := fmt.Scan(numeric)
+
+	if err != nil {
+		fmt.Println("Ошибка ввода")
+	}
+}
+
+func printValue(value float64) {
+	fmt.Printf("Курс EUR к RUB: %.2f\n", value)
 }
