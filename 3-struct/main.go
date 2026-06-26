270d9b3f5c17db40dbc50ea8b4705003294e8ffb
jnkbldgn <buldygin.em@gmail.com>
2026-06-26 16:55:55 +0300

    init

---
+++ 3-struct/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "time"
+
+type Bin struct {
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+type BinList struct {
+	Bins []Bin
+}
+
+func CreateBin(id string, private bool, createdAt time.Time, name string) (*Bin, string) {
+	if id == "" {
+		return nil, "Id is required"
+	}
+
+	if name == "" {
+		return nil, "Name is required"
+	}
+
+	var bin = Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: createdAt,
+		Name:      name,
+	}
+
+	return &bin, ""
+}
+
+func CreateBinList(bins []Bin) *BinList {
+	var binList = BinList{
+		Bins: bins,
+	}
+
+	return &binList
+}
+
+func main() {
+
+}
